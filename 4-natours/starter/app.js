ff96ced43cbabc8efa430737c8559b1e455a0c77
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2024-04-19 10:53:49 +0300

    last

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,6 +6,7 @@ const tourRouter = require('./routers/tourRoutes.js');
 const userRouter = require('./routers/userRoutes.js');
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use('/api/v2/tours', tourRouter);
 app.use('/api/v2/users', userRouter);
 
